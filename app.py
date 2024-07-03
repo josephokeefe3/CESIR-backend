8b2fb1ad4729bc8507ea919e5b5242bc104413c6
unknown <josephokeefe3@gmail.com>
2024-07-03 16:47:02 -0400

    esposing port 8000

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def submit():
     
     return jsonify({"message": "Data saved successfully!"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
